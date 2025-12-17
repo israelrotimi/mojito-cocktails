1655b043fda9218ee14348019f5616bee3bff3c6
Israel Rotimi <130306892+israelrotimi@users.noreply.github.com>
2025-12-17 06:20:37 +0000

    add Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <p>Velvet Pour</p>
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
