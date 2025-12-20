70e4bea68acdbf47d410bcebbf0ca964be371260
Israel Rotimi <izzyrotimi@gmail.com>
2025-12-20 08:19:29 +0100

    add menu (initial)

---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+
+const Menu = () => {
+  return (
+    <section id="menu" aria-labelledby="menu-heading">
+        <img src="/images/slider-left-leaf.png" alt="left leaf" id="m-left-leaf" />
+        <img src="/images/slider-right-leaf.png" alt="right leaf" id="m-right-leaf" />
+
+        <h2 id="menu-heading" className="sr-only">
+            Cocktail Menu
+        </h2>
+
+        <nav className="cocktail-tabs" aria-label="cocktail animation"></nav>
+    </section>
+  )
+}
+
+export default Menu
